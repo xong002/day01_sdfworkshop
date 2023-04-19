87a801a17407553f5048e00e28c9012e911ec624
Xin Zhi <115723006+xong002@users.noreply.github.com>
2023-04-19 15:53:17 +0800

    task 3 completed

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,6 +1,8 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -15,7 +17,7 @@ public class App {
         Path dbDir;
 
         if (args.length == 0) {
-            System.out.println("Argument is empty");
+            System.out.println("Argument is empty, default 'db' folder created");
             dbDir = Paths.get("db");
         } else {
             dbDir = Paths.get(args[0]);
@@ -37,13 +39,28 @@ public class App {
         while (!exit) {
             String input = scanner.nextLine();
 
-
             if (input.startsWith("login")) {
+                cart.clear();
                 fileName = input.substring(6, input.length()).trim();
                 dirPathFileName = dbDir + File.separator + fileName;
                 File file = new File(dirPathFileName);
                 if (file.exists()) {
-                    System.out.println("file exists");
+                    if (file.length() == 0) {
+                        System.out.println(fileName + ", your cart is empty");
+                    } else {
+                        System.out.println(fileName + ", your cart contains the following items:");
+                        FileReader fr = new FileReader(file);
+                        BufferedReader br = new BufferedReader(fr);
+                        String line;
+                        Integer index = 1;
+                        while ((line = br.readLine()) != null) {
+                            System.out.println(index.toString() + ". " + line);
+                            index++;
+                            cart.add(line);
+                        }
+                        br.close();
+                        fr.close();
+                    }
                 } else {
                     file.createNewFile();
                     System.out.println(fileName + ", your cart is empty");
@@ -54,6 +71,7 @@ public class App {
                 exit = true;
             }
 
+            // show all saved cart items + items added to 'cart'
             if (input.startsWith("list")) {
                 if (cart.isEmpty()) {
                     System.out.println("Your cart is empty");
@@ -113,16 +131,33 @@ public class App {
 
             if (input.startsWith("save")) {
                 File file = new File(dirPathFileName);
-                System.out.println(dirPathFileName);
                 if (file.exists()) {
+                    FileWriter fw = new FileWriter(file, false);
+                    String writeToFile = "";
                     for (String i : cart) {
-                        FileWriter fw = new FileWriter(file, false);
-                        fw.write(i + "\n");
-                        fw.close();
+                        writeToFile = writeToFile + i + "\n";
                     }
+                    fw.write(writeToFile);
+                    fw.close();
+                    // cart.clear();
+                    System.out.println(fileName + ", your cart has been saved");
                 } else {
                     System.out.println("Please log in as user first.");
                 }
+            }
+
+            if (input.trim().startsWith("users")) {
+                File file = dbDir.toFile();
+                File[] userList = file.listFiles();
+                // check if file exists and if dir is empty
+                if (userList.length > 0) {
+                    System.out.println("The following users are registered:");
+                    int index = 1;
+                    for (File f : userList) {
+                        System.out.println(index + ". " + f.getName());
+                        index++;
+                    }
+                } else System.out.println("No users found");
             }
         }
     }
